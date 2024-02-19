3dfaf468d1bc5a50de7a83abaf8360466ba989ba
kmr-rohit <rk972006@student.nitw.ac.in>
2024-02-19 11:50:46 +0530

    Updated UPI Service

--- src/UPI/UPI.jsx
+++ src/UPI/UPI.jsx
@@ -1,73 +1,67 @@
 import React, { useState } from "react";
 import axios from "axios";
-const baseApiUrl = "http://localhost:3000/api";
+const baseApiUrl = 'https://fastr-api-prototype-server.vercel.app/api';
 
-const UPI = () => {
+const UPI = () =>{
   const [upi_amount, setUpi_amount] = useState();
+  const [openModal, setOpenModal] = useState(false);
+  const [transactionStatusArray, setTransactionStatusArray] = useState(null);
+    
 
-  async function makeUPIpayment() {
+  async function handleUPIPayment() {
     try {
-      const {
-        data: { key },
-      } = await axios.get("http://www.localhost:3000/api/getkey");
-      const URL = baseApiUrl + "/makeupipayment";
-      const {
-        data: { order },
-      } = await axios.get(URL);
+      const { data: { key } } = await axios.get(`${baseApiUrl}/getkey`);
+      const URL = `${baseApiUrl}/makeupipayment`;
+      const { data: { order } } = await axios.get(URL);
       console.log(order);
-      const options = {
-        key,
-        amount: order.amount,
-        currency: "INR",
-        name: "Ajeet",
-        description: "Payment",
-        image: "",
-        config: {
-          display: {
-            hide: [
-              {
-                method: "card",
-              },
-              {
-                method: "wallet",
-              },
-              {
-                method: "netbanking",
-              },
-              {
-                method: "paylater",
-              },
-            ],
+  
+      // Open a new popup window for the Razorpay payment
+      const paymentWindow = window.open(`${baseApiUrl}/razorpayPayment?order_id=${order.id}&&key=${key}`, 'Razorpay', 'width=800,height=600');
+      
+      // Return a new promise that resolves when the payment status is fetched
+      const UpiPromise =  new Promise(async (resolve, reject) => {
+        // Poll to check if the window is closed
+        const pollTimer = window.setInterval(async function() {
+          if (paymentWindow.closed !== false) { // !== is required for compatibility with Opera
+            window.clearInterval(pollTimer);
+  
+            // Verify the payment status
+            try {
+              const response = await fetch(`${baseApiUrl}/upipaymentstatus`);
+              const paymentStatus = await response.json();
+              console.log(paymentStatus);
+              resolve(paymentStatus); // Resolve the outer promise with the payment status
+            } catch (error) {
+              reject(error); // Reject the outer promise if there's an error
+            }
+          }
 
-            sequence: ["block.code"], // The sequence in which blocks and methods should be shown
+        }, 200);
+      });
 
-            preferences: {
-              show_default_blocks: true, // Should Checkout show its default blocks?
-            },
-          },
-        },
-        callback_url: baseApiUrl + "/verifyupipayment",
-        order_id: order.id,
-        prefill: {
-          name: "Ajeet Verma",
-          email: "ajeet@gmail.com",
-          contact: "1234567890",
-        },
-        notes: {
-          address: "IIIT Naya Raipur",
-        },
-        theme: {
-          color: "#121212",
-        },
-      };
-
-      const razorpay = new window.Razorpay(options);
-      razorpay.open();
+      const UPiStatus = await UpiPromise;
+      //console.log(response);
+      //const UPiStatus = response[1];
+      const UPIStatusStructured = {payment_method : "UPI"  , paymentIntentId: UPiStatus.reference, status: UPiStatus.success ? 'succeeded' : 'failed'};
+      setTransactionStatusArray([UPIStatusStructured]);
+      setOpenModal(true);
     } catch (error) {
       console.error("Error:", error);
     }
+
+    
+    
+  }
+    
+  
+
+  
+  const closeModal = () => {
+    setTransactionStatusArray(null);
+    setOpenModal(false);
   }
 
+  
   return (
     <div className="upi-only-block">
       <div className="upi-only-data">
@@ -100,10 +94,26 @@ const UPI = () => {
       <button
         type="submit"
         className="btn btn-primary"
-        onClick={makeUPIpayment}
+        onClick={handleUPIPayment}
       >
         Pay Now
       </button>
+      {openModal && (
+        <div className="fixed inset-0 z-50 overflow-auto bg-gray-900 bg-opacity-50 flex items-center justify-center">
+          <div className="bg-white p-8 max-w-md mx-auto rounded-md text-center">
+            <h2 className="text-lg font-semibold mb-4">Transaction Status</h2>
+            <p className="mb-4">{transactionStatusArray && 
+              transactionStatusArray.map((status, index) => (
+                <>
+                <p key={index}>{status.payment_method} Payment with PaymentID {status.paymentIntentId} : {status.status} </p>
+                </>
+              ))
+            }</p>
+            <button onClick={closeModal} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Close</button>
+          </div>
+        </div>
+      )}
+
     </div>
   );
 };
